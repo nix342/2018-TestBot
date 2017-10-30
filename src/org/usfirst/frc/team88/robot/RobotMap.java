215c9c64accf825e20729fcf99ad98dc62ca274b
torrtoise <sjrube32@gmail.com>
2017-10-30 19:05:15 -0400

    Updated numbers for arrays + smart dashboard stuff

--- src/org/usfirst/frc/team88/robot/RobotMap.java
+++ src/org/usfirst/frc/team88/robot/RobotMap.java
@@ -17,10 +17,20 @@ public class RobotMap {
 	// public static int rangefinderPort = 1;
 	// public static int rangefinderModule = 1;
 	// Drive
+	
+	public static final int lefttalon1 = 12;
+	public static final int lefttalon2 = 11;
+	public static final int lefttalon3 = 14;
+	public static final int lefttalon4 = 8;
+	public static final int righttalon1 = 13;
+	public static final int righttalon2 = 9;
+	public static final int righttalon3 = 15;
+	public static final int righttalon4 = 10;
+	
 	public static final int shifterHigh = 0;
 	public static final int shifterLow = 1;
 	
-	public static final int[] leftTalons = {10, 2, 3, 4};
-	public static final int[] rightTalons = {5, 6, 7, 8};
+	public static final int[] leftTalons = {12, 11, 14, 8};
+	public static final int[] rightTalons = {13, 9, 15, 10};
 }
 
