99c391c327e3b0eac9f27e8a253586fc7a9695d3
Nick Avtges <navtges@gmail.com>
2017-12-13 10:26:59 -0500

    Remove unused copied code. Reformat.

--- src/org/usfirst/frc/team88/robot/subsystems/Drive.java
+++ src/org/usfirst/frc/team88/robot/subsystems/Drive.java
@@ -12,55 +12,50 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
- * Robot flies around
- * forwards, backwards, left and right
- * watch us as we go
+ * Robot flies around forwards, backwards, left and right watch us as we go
  */
 public class Drive extends Subsystem {
 
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
+	// Put methods for controlling this subsystem
+	// here. Call these from Commands.
 	private CANTalon leftTalon1, leftTalon2, leftTalon3, leftTalon4;
 	private CANTalon rightTalon1, rightTalon2, rightTalon3, rightTalon4;
 	private DoubleSolenoid shifter;
 	private final static int PROFILE = 0;
-	//private final static double P = 1.5;
+	// private final static double P = 1.5;
 	private final static double P = 0.8;
 	private final static double I = 0.0;
-	//private final static double D = 20.0;
+	// private final static double D = 20.0;
 	private final static double D = 0.0;
 	private final static double F = 1.7;
 	private final static int IZONE = 0;
 	public final static double DFT_SENSITIVITY = 0.15;
 	private final static double RAMPRATE = 80;
 	private final static double MAX_SPEED = 700;
-	
+
 	private CANTalon[] leftTalons;
 	private CANTalon[] rightTalons;
 	private CANTalon.TalonControlMode controlMode;
-	
+
 	private double maxSpeed;
 	private double targetMaxSpeed;
-	
-	public Drive(){
+
+	public Drive() {
 		System.out.println("Before loop 1");
 		leftTalons = new CANTalon[RobotMap.leftTalons.length];
 		rightTalons = new CANTalon[RobotMap.rightTalons.length];
-		
-		
-		for(int i = 0; i<RobotMap.leftTalons.length; i++){
+
+		for (int i = 0; i < RobotMap.leftTalons.length; i++) {
 			leftTalons[i] = new CANTalon(RobotMap.leftTalons[i]);
 		}
-		for(int i = 0; i<RobotMap.rightTalons.length; i++){
+		for (int i = 0; i < RobotMap.rightTalons.length; i++) {
 			rightTalons[i] = new CANTalon(RobotMap.rightTalons[i]);
 		}
 
-		
-		
-		for(int i = 0; i<RobotMap.leftTalons.length; i++){
-			if (i==0){
+		for (int i = 0; i < RobotMap.leftTalons.length; i++) {
+			if (i == 0) {
 				leftTalons[i].changeControlMode(TalonControlMode.Speed);
-				//leftTalons[i].changeControlMode(TalonControlMode.PercentVbus);
+				// leftTalons[i].changeControlMode(TalonControlMode.PercentVbus);
 				leftTalons[i].setFeedbackDevice(CANTalon.FeedbackDevice.QuadEncoder);
 				leftTalons[i].configEncoderCodesPerRev(512);
 				leftTalons[i].setPID(P, I, D, F, IZONE, RAMPRATE, PROFILE);
@@ -71,17 +66,17 @@ public class Drive extends Subsystem {
 			} else {
 				leftTalons[i].changeControlMode(TalonControlMode.Follower);
 				leftTalons[i].set(RobotMap.leftTalons[0]);
-				
+
 			}
 			leftTalons[i].enableBrakeMode(true);
 		}
 		System.out.println("After loop 1");
 		System.out.println(RobotMap.rightTalons.length);
-		
-		for(int i = 0; i<RobotMap.rightTalons.length; i++){
-			if (i==0){
+
+		for (int i = 0; i < RobotMap.rightTalons.length; i++) {
+			if (i == 0) {
 				rightTalons[i].changeControlMode(TalonControlMode.Speed);
-				//rightTalons[i].changeControlMode(TalonControlMode.PercentVbus);
+				// rightTalons[i].changeControlMode(TalonControlMode.PercentVbus);
 				rightTalons[i].setFeedbackDevice(CANTalon.FeedbackDevice.QuadEncoder);
 				rightTalons[i].configEncoderCodesPerRev(512);
 				rightTalons[i].setPID(P, I, D, F, IZONE, RAMPRATE, PROFILE);
@@ -92,35 +87,30 @@ public class Drive extends Subsystem {
 			} else {
 				rightTalons[i].changeControlMode(TalonControlMode.Follower);
 				rightTalons[i].set(RobotMap.rightTalons[0]);
-				
+
 			}
 			rightTalons[i].enableBrakeMode(true);
 		}
-		
-		leftTalons[0].setEncPosition(0);
-		rightTalons[0].setEncPosition(0);
-		enableRampRate();
-		
-		shifter = new DoubleSolenoid(RobotMap.shifterLow,RobotMap.shifterHigh);
+
+		shifter = new DoubleSolenoid(RobotMap.shifterLow, RobotMap.shifterHigh);
 		shifter.set(Value.kReverse);
-		
+
 		resetEncoders();
 	}
-	
-	
-	public void wheelSpeed (double left, double right){
-		leftTalons[0].set(-left*MAX_SPEED);
-		rightTalons[0].set(-right*MAX_SPEED);
-		//leftTalons[0].set(left);
-		//rightTalons[0].set(-right);
+
+	public void wheelSpeed(double left, double right) {
+		leftTalons[0].set(-left * MAX_SPEED);
+		rightTalons[0].set(-right * MAX_SPEED);
+		// leftTalons[0].set(left);
+		// rightTalons[0].set(-right);
 
 	}
-	
+
 	public void resetEncoders() {
 		leftTalons[0].setPosition(0);
 		rightTalons[0].setPosition(0);
 	}
-	
+
 	public boolean isLowGear() {
 		if (shifter.get() == Value.kReverse) {
 			return true;
@@ -135,131 +125,43 @@ public class Drive extends Subsystem {
 	public int getRightEncPosition() {
 		return rightTalons[0].getEncPosition();
 	}
-	
-    public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        setDefaultCommand(new DriveTank());
-    }
-    
-    public double getAvgPosition() {
+
+	public void initDefaultCommand() {
+		// Set the default command for a subsystem here.
+		setDefaultCommand(new DriveTank());
+	}
+
+	public double getAvgPosition() {
 		return (leftTalons[0].getPosition() + rightTalons[0].getPosition()) / 2.0;
 	}
-    
-    public double getAvgSpeed() {
+
+	public double getAvgSpeed() {
 		double speed = (leftTalons[0].getSpeed() + rightTalons[0].getSpeed()) / 2;
 
 		return speed;
 	}
-    public void driveCurve(double outputMagnitude, double curve) {
-		driveCurve(outputMagnitude, curve, DFT_SENSITIVITY);
 
-	}
-    public void driveCurve(double outputMagnitude, double curve, double sensitivity) {
-		final double leftOutput;
-		final double rightOutput;
-		final double minimum = 0.02;
-		final double minRange = 0.008;
+	public void shift() {
 
-		if (outputMagnitude == 0 && Math.abs(getAvgSpeed()) < 200) {
-			if (curve < minRange && curve > -minRange) {
-				curve = 0;
-			} else if (curve < minimum && curve > 0) {
-				curve = minimum;
-			} else if (curve > -minimum && curve < 0) {
-				curve = -minimum;
-			}
-
-			leftOutput = curve * (isLowGear() ? 0.5 : 1.0);
-			rightOutput = -curve * (isLowGear() ? 0.5 : 1.0);
-		} else if (curve < 0) {
-			double value = Math.log(-curve);
-			double ratio = (value - sensitivity) / (value + sensitivity);
-			if (ratio == 0) {
-				ratio = .0000000001;
-			}
-			leftOutput = outputMagnitude / ratio;
-			rightOutput = outputMagnitude;
-		} else if (curve > 0) {
-			double value = Math.log(curve);
-			double ratio = (value - sensitivity) / (value + sensitivity);
-			if (ratio == 0) {
-				ratio = .0000000001;
-			}
-			leftOutput = outputMagnitude;
-			rightOutput = outputMagnitude / ratio;
+		// SECRET COMMENT - KYLE H
+		if (shifter.get() == (Value.kForward)) {
+			shifter.set(Value.kReverse);
 		} else {
-			leftOutput = outputMagnitude;
-			rightOutput = outputMagnitude;
-		}
-
-		setTarget(leftOutput, rightOutput);
-	}
-	public void disableRampRate() {
-		leftTalons[0].setVoltageRampRate(0.0);
-		rightTalons[0].setVoltageRampRate(0.0);
-	}
-
-	public void enableRampRate() {
-		leftTalons[0].setVoltageRampRate(RAMPRATE);
-		rightTalons[0].setVoltageRampRate(RAMPRATE);
-	}
-    public void setTarget(double left, double right) {
-		double currentMaxSpeed;
-
-		switch (controlMode) {
-		case PercentVbus:
-			leftTalons[0].set(left);
-			rightTalons[0].set(-right);
-			break;
-		case Speed:
-			currentMaxSpeed = getMaxSpeed();
-			leftTalons[0].set(left * currentMaxSpeed);
-			rightTalons[0].set(right * currentMaxSpeed);
-			break;
-		case Position:
-		case Disabled:
-		default:
-			break;
+			shifter.set(Value.kForward);
 		}
 	}
-    private double getMaxSpeed() {
-		// note that this adjusts maxSpeed every time it is called
-		// and so should only be called once per setTarget call
-		if (targetMaxSpeed > maxSpeed) {
-			maxSpeed += 50;
-			if (maxSpeed > targetMaxSpeed) {
-				maxSpeed = targetMaxSpeed;
-			}
-		} else if (targetMaxSpeed < maxSpeed) {
-			maxSpeed -= 50;
-			if (maxSpeed < targetMaxSpeed) {
-				maxSpeed = targetMaxSpeed;
-			}
-		}
-		return maxSpeed;
-	}
-    public void shift() {
-	    
-	    // SECRET COMMENT - KYLE H
-    	if (shifter.get() == (Value.kForward)){
-    		shifter.set(Value.kReverse);	
-    	}
-    	else {
-    		shifter.set(Value.kForward);
-    	}
-    }
-	
-    public void updateDashboard(){
-    	SmartDashboard.putNumber("LeftError: ", leftTalons[0].getError());
-    	SmartDashboard.putNumber("LeftSetpoint: ", leftTalons[0].getSetpoint());
-    	SmartDashboard.putNumber("LeftEncPosition: ", leftTalons[0].getEncPosition());
-    	SmartDashboard.putNumber("LeftPosition: ", leftTalons[0].getPosition());
+
+	public void updateDashboard() {
+		SmartDashboard.putNumber("LeftError: ", leftTalons[0].getError());
+		SmartDashboard.putNumber("LeftSetpoint: ", leftTalons[0].getSetpoint());
+		SmartDashboard.putNumber("LeftEncPosition: ", leftTalons[0].getEncPosition());
+		SmartDashboard.putNumber("LeftPosition: ", leftTalons[0].getPosition());
 		SmartDashboard.putNumber("LeftSpeed: ", leftTalons[0].getSpeed());
 		SmartDashboard.putNumber("LeftEncVel:", leftTalons[0].getEncVelocity());
 		SmartDashboard.putNumber("RightError: ", rightTalons[0].getError());
-    	SmartDashboard.putNumber("RightSetpoint: ", rightTalons[0].getSetpoint());
-    	SmartDashboard.putNumber("RightEncPosition: ", rightTalons[0].getEncPosition());
-    	SmartDashboard.putNumber("RightPosition: ", rightTalons[0].getPosition());
+		SmartDashboard.putNumber("RightSetpoint: ", rightTalons[0].getSetpoint());
+		SmartDashboard.putNumber("RightEncPosition: ", rightTalons[0].getEncPosition());
+		SmartDashboard.putNumber("RightPosition: ", rightTalons[0].getPosition());
 		SmartDashboard.putNumber("RightSpeed: ", rightTalons[0].getSpeed());
 		SmartDashboard.putNumber("RightEncVel:", rightTalons[0].getEncVelocity());
 		SmartDashboard.putBoolean("LowGear:", isLowGear());
@@ -272,6 +174,5 @@ public class Drive extends Subsystem {
 			SmartDashboard.putNumber("RightCurrent" + i, rightTalons[i].getOutputCurrent());
 			SmartDashboard.putNumber("RightVoltage" + i, rightTalons[i].getOutputVoltage());
 		}
-    }
+	}
 }
-
