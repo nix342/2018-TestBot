3d8e1b70b55724645edb7c3933ce00042c8abdf3
Allison Turner <hermione2259@gmail.com>
2017-12-19 19:10:15 -0500

    Added constants to check the capabilities of the robot. This will allow us to quickly change between looping modes based on robot abilities.

--- src/org/usfirst/frc/team88/robot/subsystems/Drive.java
+++ src/org/usfirst/frc/team88/robot/subsystems/Drive.java
@@ -28,6 +28,8 @@ public class Drive extends Subsystem {
 	private final static int IZONE = 0;
 	private final static double RAMPRATE = 80;
 	private final static double MAX_SPEED = 700;
+	private final static boolean CAN_SHIFT = false;
+	private final static boolean CAN_CLOSED_LOOP = false;
 
 	private CANTalon[] leftTalons;
 	private CANTalon[] rightTalons;
@@ -40,58 +42,85 @@ public class Drive extends Subsystem {
 		for (int i = 0; i < RobotMap.leftTalons.length; i++) {
 			leftTalons[i] = new CANTalon(RobotMap.leftTalons[i]);
 		}
-		
+
 		for (int i = 0; i < RobotMap.rightTalons.length; i++) {
 			rightTalons[i] = new CANTalon(RobotMap.rightTalons[i]);
 		}
-
-		for (int i = 0; i < RobotMap.leftTalons.length; i++) {
-			if (i == 0) {
-				leftTalons[i].changeControlMode(TalonControlMode.Speed);
-				// leftTalons[i].changeControlMode(TalonControlMode.PercentVbus);
-				leftTalons[i].setFeedbackDevice(CANTalon.FeedbackDevice.QuadEncoder);
-				leftTalons[i].configEncoderCodesPerRev(512);
-				leftTalons[i].setPID(P, I, D, F, IZONE, RAMPRATE, PROFILE);
-				leftTalons[i].reverseSensor(true);
-				leftTalons[i].reverseOutput(true);
-				leftTalons[i].configNominalOutputVoltage(+0.0f, -0.0f);
-				leftTalons[i].configPeakOutputVoltage(+10.0f, -10.0f);
-			} else {
-				leftTalons[i].changeControlMode(TalonControlMode.Follower);
-				leftTalons[i].set(RobotMap.leftTalons[0]);
+		if(CAN_CLOSED_LOOP){
+			for (int i = 0; i < RobotMap.leftTalons.length; i++) {
+				if (i == 0) {
+					leftTalons[i].changeControlMode(TalonControlMode.Speed);
+					// leftTalons[i].changeControlMode(TalonControlMode.PercentVbus);
+					leftTalons[i].setFeedbackDevice(CANTalon.FeedbackDevice.QuadEncoder);
+					leftTalons[i].configEncoderCodesPerRev(512);
+					leftTalons[i].setPID(P, I, D, F, IZONE, RAMPRATE, PROFILE);
+					leftTalons[i].reverseSensor(true);
+					leftTalons[i].reverseOutput(true);
+					leftTalons[i].configNominalOutputVoltage(+0.0f, -0.0f);
+					leftTalons[i].configPeakOutputVoltage(+10.0f, -10.0f);
+				} else {
+					leftTalons[i].changeControlMode(TalonControlMode.Follower);
+					leftTalons[i].set(RobotMap.leftTalons[0]);
+				}
+				leftTalons[i].enableBrakeMode(true);
 			}
-			leftTalons[i].enableBrakeMode(true);
+
+
+			for (int i = 0; i < RobotMap.rightTalons.length; i++) {
+				if (i == 0) {
+					rightTalons[i].changeControlMode(TalonControlMode.Speed);
+					// rightTalons[i].changeControlMode(TalonControlMode.PercentVbus);
+					rightTalons[i].setFeedbackDevice(CANTalon.FeedbackDevice.QuadEncoder);
+					rightTalons[i].configEncoderCodesPerRev(512);
+					rightTalons[i].setPID(P, I, D, F, IZONE, RAMPRATE, PROFILE);
+					rightTalons[i].reverseSensor(true);
+					rightTalons[i].reverseOutput(false);
+					rightTalons[i].configNominalOutputVoltage(+0.0f, -0.0f);
+					rightTalons[i].configPeakOutputVoltage(+10.0f, -10.0f);
+				} else {
+					rightTalons[i].changeControlMode(TalonControlMode.Follower);
+					rightTalons[i].set(RobotMap.rightTalons[0]);
+				}
+				rightTalons[i].enableBrakeMode(true);
+			}
+			resetEncoders();
+		}
+		else{
+			for (int i = 0; i < RobotMap.leftTalons.length; i++) {
+				if (i == 0) {
+					leftTalons[i].changeControlMode(TalonControlMode.PercentVbus);
+				} else {
+					leftTalons[i].changeControlMode(TalonControlMode.Follower);
+					leftTalons[i].set(RobotMap.leftTalons[0]);
+				}
+				leftTalons[i].enableBrakeMode(true);
+			}
+			for (int i = 0; i < RobotMap.rightTalons.length; i++) {
+				if (i == 0) {
+					rightTalons[i].changeControlMode(TalonControlMode.PercentVbus);
+				} else {
+					rightTalons[i].changeControlMode(TalonControlMode.Follower);
+					rightTalons[i].set(RobotMap.rightTalons[0]);
+				}
+				rightTalons[i].enableBrakeMode(true);
+			}
+		}
+		if(CAN_SHIFT){
+			shifter = new DoubleSolenoid(RobotMap.shifterLow, RobotMap.shifterHigh);
+			shifter.set(Value.kReverse);
 		}
 
-		for (int i = 0; i < RobotMap.rightTalons.length; i++) {
-			if (i == 0) {
-				rightTalons[i].changeControlMode(TalonControlMode.Speed);
-				// rightTalons[i].changeControlMode(TalonControlMode.PercentVbus);
-				rightTalons[i].setFeedbackDevice(CANTalon.FeedbackDevice.QuadEncoder);
-				rightTalons[i].configEncoderCodesPerRev(512);
-				rightTalons[i].setPID(P, I, D, F, IZONE, RAMPRATE, PROFILE);
-				rightTalons[i].reverseSensor(true);
-				rightTalons[i].reverseOutput(false);
-				rightTalons[i].configNominalOutputVoltage(+0.0f, -0.0f);
-				rightTalons[i].configPeakOutputVoltage(+10.0f, -10.0f);
-			} else {
-				rightTalons[i].changeControlMode(TalonControlMode.Follower);
-				rightTalons[i].set(RobotMap.rightTalons[0]);
-			}
-			rightTalons[i].enableBrakeMode(true);
-		}
-
-		shifter = new DoubleSolenoid(RobotMap.shifterLow, RobotMap.shifterHigh);
-		shifter.set(Value.kReverse);
-
-		resetEncoders();
 	}
 
 	public void wheelSpeed(double left, double right) {
-		leftTalons[0].set(-left * MAX_SPEED);
-		rightTalons[0].set(-right * MAX_SPEED);
-		// leftTalons[0].set(left);
-		// rightTalons[0].set(-right);
+		if(CAN_CLOSED_LOOP){
+			leftTalons[0].set(-left * MAX_SPEED);
+			rightTalons[0].set(-right * MAX_SPEED);
+		}
+		else{
+			leftTalons[0].set(-left);
+			rightTalons[0].set(right);
+		}
 	}
 
 	public void resetEncoders() {
@@ -100,11 +129,14 @@ public class Drive extends Subsystem {
 	}
 
 	public boolean isLowGear() {
-		if (shifter.get() == Value.kReverse) {
-			return true;
-		} else {
-			return false;
+		if(CAN_SHIFT){
+			if (shifter.get() == Value.kReverse) {
+				return true;
+			} else {
+				return false;
+			}
 		}
+		return false;
 	}
 
 	public int getLeftEncPosition() {
@@ -126,14 +158,17 @@ public class Drive extends Subsystem {
 	}
 
 	public void shift() {
-		if (shifter.get() == (Value.kForward)) {
-			shifter.set(Value.kReverse);
-		} else {
-			shifter.set(Value.kForward);
+		if(CAN_SHIFT){
+			if (shifter.get() == (Value.kForward)) {
+				shifter.set(Value.kReverse);
+			} else {
+				shifter.set(Value.kForward);
+			}
 		}
 	}
 
 	public void updateDashboard() {
+
 		SmartDashboard.putNumber("LeftError: ", leftTalons[0].getError());
 		SmartDashboard.putNumber("LeftSetpoint: ", leftTalons[0].getSetpoint());
 		SmartDashboard.putNumber("LeftEncPosition: ", leftTalons[0].getEncPosition());
@@ -148,6 +183,7 @@ public class Drive extends Subsystem {
 		SmartDashboard.putNumber("RightEncVel:", rightTalons[0].getEncVelocity());
 		SmartDashboard.putBoolean("LowGear:", isLowGear());
 		SmartDashboard.putNumber("AvgPosition", getAvgPosition());
+
 		for (int i = 0; i < RobotMap.leftTalons.length; i++) {
 			SmartDashboard.putNumber("LeftCurrent" + i, leftTalons[i].getOutputCurrent());
 			SmartDashboard.putNumber("LeftVoltage" + i, leftTalons[i].getOutputVoltage());
